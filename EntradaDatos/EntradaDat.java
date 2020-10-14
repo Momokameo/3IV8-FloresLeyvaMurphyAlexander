5b5758cf4a0f2557219e8a36577c8028223e40b9
Momokameo <mfloresl1901@alumno.ipn.mx>
2020-10-14 16:08:21 -0500

    HolaMundo

---
+++ EntradaDatos/EntradaDat.java
@@ -0,0 +1,9 @@
+class EntradaTexto {
+public static void main(String[] args){
+//variables 
+ String nombre;
+ System.out.println("Dime tu nombre:");
+ nombre = System.console().readLine();
+ System.out.println("Hola " +nombre+ " bienvenido a tu segundo programa");
+}
+}
---
+++ HolaMundo/HolaMundo.java
@@ -0,0 +1,6 @@
+class HolaMundo{
+ public static void main(String[]args){
+     System.out.print("Hola Mundo Aquitamos");
+     //lo mismo que "escribir"
+ }
+}
